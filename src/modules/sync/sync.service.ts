3b8e9370fb18d1ac0c7081ca803784d8c8d7f75b
jacob <jacob@jacobdeMacBook-Pro.local>
2025-06-01 20:51:00 +0800

    fix: 🐛 fix concurrent

--- src/modules/sync/sync.service.ts
+++ src/modules/sync/sync.service.ts
@@ -16,14 +16,14 @@ import { TxEntity } from './tx.entity';
 
 // 创建队列实例
 const blockQueue = new Bull('block-queue', {
-  concurrent: 50, // 降低并发数，避免产生太多tx任务
+  concurrent: 10, // 降低并发数，避免产生太多tx任务
   maxRetries: 3,
   retryDelay: 1000,
   dataDir: '.queue',
   loadPersisted: true,
 });
 const txQueue = new Bull('tx-queue', {
-  concurrent: 100, // 降低并发数，避免内存压力
+  concurrent: 30, // 降低并发数，避免内存压力
   maxRetries: 3,
   retryDelay: 1000,
   dataDir: '.queue',
